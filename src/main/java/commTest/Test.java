4092c229dd6dd9cd16dbffd96c997b43722f84fa
suhengf <893793211@qq.com>
2021-03-08 18:09:44 +0800

    上海海事大学代码提交

--- src/main/java/commTest/Test.java
+++ src/main/java/commTest/Test.java
@@ -3,12 +3,19 @@ package commTest;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println((new Date().toLocaleString()));
-        System.out.println(getNowDate());
-
+//        System.out.println((new Date().toLocaleString()));
+//        System.out.println(getNowDate());
+        Map<String,Integer> map = new HashMap<>();
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+        Integer c = map.get("d");
+        System.out.println(c);
     }
 
     public static Date getNowDate() {
