0f86189a54de63fdca561ce6829a7cd6a247371b
suhengf <893793211@qq.com>
2021-03-11 22:28:21 +0800

    上海海事大学代码提交

--- src/main/java/shanghaimaritimeuniversity/AskInfoHandler.java
+++ src/main/java/shanghaimaritimeuniversity/AskInfoHandler.java
@@ -3,24 +3,16 @@ package shanghaimaritimeuniversity;
 import commTest.WriteToFile;
 import ecust.UserInfo;
 import ecust.WebDriverUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-import static utils.QRtool.elementSnapshot;
-import static utils.QRtool.recognize;
-
+@Slf4j
 public class AskInfoHandler {
-    private static org.slf4j.Logger logger = LoggerFactory.getLogger(AskInfoHandler.class);
 
     public static void handler(UserInfo userInfo, WebDriver driver,int conut) throws Exception {
 
@@ -48,8 +40,8 @@ public class AskInfoHandler {
             driver.get(url);
             driver.findElement(By.xpath("/html/body/div[4]/div/form/div[1]/ul[1]/li[1]/input[4]")).sendKeys(userInfo.getUserId().trim());
             driver.findElement(By.xpath("/html/body/div[4]/div/form/div[1]/ul[1]/li[2]/input")).sendKeys(userInfo.getPassword().trim());
-            logger.info("current userId：" + userInfo.getUserId());
-            Thread.sleep(20000);
+            log.info("current userId：" + userInfo.getUserId());
+            Thread.sleep(6000);
 
             driver.findElement(By.xpath("/html/body/div[4]/div/form/div[1]/span")).click();
             Thread.sleep(6000);
@@ -59,10 +51,8 @@ public class AskInfoHandler {
             handleAskInfo(driver, userInfo);
 
         } catch (Exception e) {
-            logger.error("用户:"+userInfo.getUserId()+"密码:"+userInfo.getPassword());
-            StringBuilder sb = new StringBuilder();
-            sb.append("账号 （验证码不对）           ").append(userInfo.getUserId()).append("         密码    ").append(userInfo.getPassword()).append("       错误 ").append("\n");
-            WriteToFile.readTxtFile(sb.toString(), new File("src\\main\\files\\error_0.txt"));
+            log.error("用户:"+userInfo.getUserId()+"密码:"+userInfo.getPassword());
+
         }
         driver.quit();
 
@@ -75,111 +65,30 @@ public class AskInfoHandler {
             alert.accept();
             alert.dismiss();
         } catch (Exception e) {
-            logger.info("关闭窗口");
+            log.info("关闭窗口");
         }
 
     }
 
     private static void handleAskInfo( WebDriver driver,UserInfo userInfo)  throws Exception{
         //基本信息查看
-        driver.switchTo().defaultContent();
-        switchleftFrame(driver);
-        driver.findElement(By.xpath("/html/body/form/table/tbody/tr/td/table[2]/tbody/tr/td/div/div[1]/table[1]/tbody/tr/td[4]/a")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[2]/div[1]/div[3]/div/div[1]/div[2]/ul[1]/div/dl/dt/div[1]/div[1]")).click();
+       Thread.sleep(5000);
+//                    /html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div/ul[2]/div[1]/ul/li[2]/div/b
+       String str = "/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div/ul[1]/div[";
+        for (int i = 1; i <20 ; i++) {
+            if (WebDriverUtils.check(driver, By.xpath(str+i+"]/li/div/div/b"))) {
 
-        switchCenterFrame(driver);
-        //获取准考证值
-        String lblCertificate = driver.findElement(By.id("lblCertificate")).getText();
-        logger.info("准考证号码: "+lblCertificate);
-
-
-        switchleftFrame(driver);
-        //成绩信息查看
-        driver.findElement(By.id("tv_Leftt2")).click();
-        Thread.sleep(2000);
-        switchCenterFrame(driver);
-        //学号
-        String lblStuNum = driver.findElement(By.id("lblStuNum")).getText();
-        logger.info("学号: " + lblStuNum);
-        //姓名
-        String lblStuName = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[5]/span")).getText();
-
-        logger.info("姓名: " + lblStuName);
-
-        List<Map<String, String>> maps = queryCourse(driver);
-
-        if (!maps.isEmpty()) {
-            maps.forEach(map -> {
-
-                StringBuilder sb = new StringBuilder();
-                sb.append("账号            ").append(userInfo.getUserId()).append("         密码   ").append(userInfo.getPassword()).append(lblStuNum).append("                ").append(lblStuName).append("             ").append(map.get("报名批次"))
-                        .append("                    ").append(map.get("考试科目")).append("            ").append(map.get("通过情况")).append("         ").append(map.get("得分")).append("\n");
-                try {
-                    WriteToFile.readTxtFile(sb.toString(), new File("src\\main\\files\\new_0.txt"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            });
-        } else {
-            StringBuilder sb = new StringBuilder();
-            sb.append("账号            ").append(userInfo.getUserId()).append("         密码   ").append("                                        ").append(userInfo.getPassword()).append("                                        ").append(lblStuNum).append("            ").append(lblStuName).append("        ").append("\n");
-            WriteToFile.readTxtFile(sb.toString(), new File("src\\main\\files\\noResult_0.txt"));
-        }
-
-
-    }
-
-
-        public static void switchFrame(String iframe, WebDriver driver){
-                WebElement webElement0 = driver.findElement(By.xpath(iframe));
-                driver.switchTo().frame(webElement0);
-
-        }
-
-
-    public static void switchleftFrame( WebDriver driver){
-        driver.switchTo().parentFrame();
-        switchFrame("/html/frameset/frameset/frame[1]",driver);
-
-    }
-
-
-    public static void switchCenterFrame(WebDriver driver) {
-        driver.switchTo().parentFrame();
-        switchFrame("/html/frameset/frameset/frame[2]", driver);
-
-    }
-
-    //2020批次成绩查询
-    public static List<Map<String, String>> queryCourse(WebDriver driver) {
-
-        List<Map<String, String>> list = new ArrayList<>();
-
-        for (int i = 0; i < 20; i++) {
-            ConcurrentHashMap<String, String> map = new ConcurrentHashMap();
-            String batchXpath = "/html/body/form/table/tbody/tr[3]/td/div/table/tbody/tr["+i+"]";
-            if (WebDriverUtils.check(driver, By.xpath(batchXpath + "/td[1]"))) {
-
-                if ("2020年12月统考".equals(driver.findElement(By.xpath(batchXpath + "/td[1]")).getText())) {
-                    //2020年12月统考
-                    map.put("报名批次", driver.findElement(By.xpath(batchXpath + "/td[1]")).getText());
-                    //考试科目
-                    map.put("考试科目", driver.findElement(By.xpath(batchXpath + "/td[2]")).getText());
-                    //是否合格
-                    map.put("通过情况", driver.findElement(By.xpath(batchXpath + "/td[3]")).getText());
-                    //得分
-                    map.put("得分", driver.findElement(By.xpath(batchXpath + "/td[4]")).getText());
-                    logger.info(String.valueOf(map));
-                    list.add(map);
-                }
+                //判断是否播放完成
+                //  /html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div/ul[1]/div[1]/li/div/div/b[2]
 
             }
-
         }
-        return list;
+
 
     }
+
+
 
 
 }
