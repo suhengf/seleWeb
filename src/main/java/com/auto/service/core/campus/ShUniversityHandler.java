cb746ea35af5952963a73df68c148ba84bf5c13b
suhengf <893793211@qq.com>
2021-05-18 19:13:24 +0800

    上海大学代码开发

--- src/main/java/com/auto/service/core/campus/ShUniversityHandler.java
+++ src/main/java/com/auto/service/core/campus/ShUniversityHandler.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
+
 @Slf4j
 @Component
 public class ShUniversityHandler  implements CampusOnlineHandler {
@@ -16,12 +17,22 @@ public class ShUniversityHandler  implements CampusOnlineHandler {
     public void onlineProcess(UserInfo userInfo, WebDriver driver, int course) throws Exception {
         log.info("上海大学逻辑处理");
         Thread.sleep(30000);
+        //课程信息
         driver.findElement(By.xpath("/html/body/div[2]/div[3]/table/tbody/tr/td[1]/div[2]/div/div[2]/div[2]/ul/li[4]/a")).click();
         Thread.sleep(30000);
+        //网上学习
         String str = "/html/body/div[2]/div[3]/table/tbody/tr/td[2]/div/div[3]/div/table[2]/tbody/tr[";
         for (int i = 3; i < 15; i++) {
-            if (WebDriverUtils.check(driver, By.xpath(str+"]/td[10]/a"))) {
-                driver.findElement(By.xpath(str+"]/td[10]/a")).click();
+            String onlineStudy = str+i+"]/td[10]/a";
+            if (WebDriverUtils.check(driver, By.xpath(onlineStudy))) {
+                String text = str+i+"]/td[2]";
+                String courseTitle = driver.findElement(By.xpath(onlineStudy)).getText();
+                if(text.contains("现代物流管理")){
+                    driver.findElement(By.xpath(onlineStudy)).click();
+                    //视频逻辑处理
+                    courseHandle(driver,courseTitle);
+                }
+
             }
 
         }
@@ -29,6 +40,22 @@ public class ShUniversityHandler  implements CampusOnlineHandler {
         log.info("上海大学逻辑处理结束");
 
     }
+
+
+    public void courseHandle(WebDriver driver,String courseTitle) throws InterruptedException {
+        //进入学习进度页面  重新开了个页面
+        WebDriverUtils.switchToWindowByTitle(driver,courseTitle+" - 首页");
+        Thread.sleep(20000);
+        //开始学习
+        String startStudy = "/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div/div/div/div/table/tbody/tr/td[1]/div[1]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr/td[3]/button";
+        if (WebDriverUtils.check(driver, By.xpath(startStudy))) {
+            driver.findElement(By.xpath(startStudy)).click();
+            //视频处理逻辑
+        }
+
+
+    }
+
 
     @Override
     public EnumUniversityName universityName() {
