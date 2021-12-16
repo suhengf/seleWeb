1bf019ad49351321bdc9c0bd7390db4a44b5534f
suhengf <893793211@qq.com>
2021-12-17 00:48:05 +0800

    基于 习近平新时代中国特色社会主义思想 科目类 代码逻辑的书写

--- selenium/src/main/java/com/auto/service/core/campus/MaritimeUniversityJourneyHandler.java
+++ selenium/src/main/java/com/auto/service/core/campus/MaritimeUniversityJourneyHandler.java
@@ -29,13 +29,15 @@ public class MaritimeUniversityJourneyHandler implements CampusOnlineHandler {
             driver.findElement(By.xpath(task)).click();
             Thread.sleep(3000);
 
-            // 点击形考任务
-            driver.findElement(By.xpath("/html/body/div[2]/header/ul[1]/li[2]/a")).click();
 
             //页面跳转
             WebDriverUtils.switchToWindowByTitle(driver, courseName);
             log.info("切换 {} 界面之后 开始执行逻辑处理",courseName);
-            executeHouseWork(driver);
+            // 点击形考任务
+            driver.findElement(By.xpath("/html/body/div[2]/header/ul[1]/li[2]/a")).click();
+
+
+            executeHouseWork(driver,courseName);
         }
 
 
@@ -48,19 +50,52 @@ public class MaritimeUniversityJourneyHandler implements CampusOnlineHandler {
      * 处理形考任务
      * @param driver
      */
-    private void executeHouseWork(WebDriver driver) throws InterruptedException {
+    private void executeHouseWork(WebDriver driver,String courseName) throws Exception {
         //1.形考任务 /html/body/div[2]/div[3]/div/div/div[1]/div/div[3]/a
         //2.形考任务 /html/body/div[2]/div[3]/div/div/div[2]/div/div[3]/a
+
         String houseWor ="/html/body/div[2]/div[3]/div/div/div[";
-        for (int i = 1; i < 10; i++) {
-            houseWor=  houseWor+"]/div/div[3]/a";
-            driver.findElement(By.xpath(houseWor)).click();
+        for (int i = 2; i < 10; i++) {
+            //进去形考
+            houseWor = houseWor+i+"]/div/div[3]/a";
+            Thread.sleep(4000);
+            WebDriverUtils.threeClick(driver,houseWor,10);
+
             Thread.sleep(3000);
-            //处理作业任务
-            //点击上次试答
-            driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[3]/div/section/div/div/div[3]/div/form/button")).click();
+            //处理作业任务                /html/body/div[3]/div[2]/div/div/section/div/div/div[2]/div/form/button
+            //点击上次试答    开始答题            /html/body/div[3]/div[2]/div/div/section/div/div/div[2]/div/form/button
+            driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/section/div/div/div[2]/div/form/button")).click();
+            doHomeWork(driver);
+
+        }
 
 
+
+
+
+
+    }
+
+
+    private void doHomeWork(WebDriver driver) throws InterruptedException {
+
+        for (int i = 2; i < 60; i++) {
+            String problem = "/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div["+ i+"]/div[2]/div/div[1]/p";
+            if (WebDriverUtils.check(driver, By.xpath(problem))) {
+                String proChinese = driver.findElement(By.xpath(problem)).getText();
+                log.info("proChinese:{}",proChinese);
+
+            }
+        }
+
+
+
+        String nextPage = "/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div[4]/input";
+        repeatDo(driver,nextPage);
+
+        String finishPage  ="/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div[4]/input[2]";
+        if (WebDriverUtils.check(driver, By.xpath(finishPage))) {
+            driver.findElement(By.xpath(finishPage)).click();
         }
 
 
@@ -70,9 +105,38 @@ public class MaritimeUniversityJourneyHandler implements CampusOnlineHandler {
 
 
 
+    public void repeatDo(WebDriver driver,String nextPage)throws InterruptedException{
+        //下一页
+        nextPage =  WebDriverUtils.check(driver, By.xpath(nextPage))?nextPage:"/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div[11]/input";
+        if (WebDriverUtils.check(driver, By.xpath(nextPage))) {
+            String nextPageChinese = driver.findElement(By.xpath(nextPage)).getText();
+            log.info("nextPageChinese:{}",nextPageChinese);
+            Thread.sleep(5000);
+            for (int i = 2; i < 60; i++) {
+
+                final String text = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div[1]/div[2]/div/div/p")).getText();
+                String problem = "/html/body/div[3]/div[2]/div/div/section[1]/div/div/form/div/div["+ i+"]/div[2]/div/div[1]/p";
+                if (WebDriverUtils.check(driver, By.xpath(problem))) {
+
+                    //逻辑抽离  整理答案
+                    String proChinese = driver.findElement(By.xpath(problem)).getText();
+                    log.info("proChinese:{}",proChinese);
+
+
+                }
+            }
+        }else{
+            return;
+        }
+
+    }
 
 
 
+    //匹配答题  根据题目类型 分为 判断题 多选题  单选题
+    public void  answerQue(){
+
+    }
 
 
 
